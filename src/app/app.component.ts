ac051a0ce9b8908ce5cd5b64737a69bcbc2a0617
aidenhemmings <agjhemmings@gmail.com>
2025-09-28 12:56:33 +0200

    Test v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    console.log('AppComponent initialized');
+  }
 }
